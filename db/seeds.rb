5bad46ec2981d4a7295f893541d4816cd4a7ab27
biglam <craig@techiteasy.org.uk>
2016-01-13 16:24:37 +0000

    seeded place types

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.delete_all
+Person.delete_all
+Memory.delete_all
+Place.delete_all
+Placetype.delete_all
 
 admin = User.create(email: "admin@admin.example", username: "admin", password: "password", role: "admin")
 normaluser = User.create(email: "user@user.example", username: "user", password: "password")
+
+Person.create(firstname: "Joe", lastname: "Bloggs", dob: "2015/01/01")
+
+["Home", "Town", "Restaurant or Bar", "Areena", "Sports Hall or Stadium", "Landmark", "Countryside"].each do |placetype|
+	Placetype.create(placetype: "#{placetype}")
+end
