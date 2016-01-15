6d6a54aae1bebac6ed59caa301d33347e2f0580f
Craig Lamont <craig@techiteasy.org.uk>
2016-01-15 11:49:54 +0000

    users show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_one :memory
+  has_many :memories
   has_many :people_adminstrations, foreign_key: "adminstrator_id"
   has_many :adminstered_people, through: :people_adminstrations, source: :person
+
+  def average_ranking
+    if memories.count > 0
+      rankings = memories.map {|m| m.ranking }
+      (rankings.inject{ |sum, el| sum + el }.to_f / rankings.size).round
+    else
+      0
+    end
+  end
 end
