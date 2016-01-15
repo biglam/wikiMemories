d11791c550b0967692fac8985f58bd535b076994
biglam <craig@techiteasy.org.uk>
2016-01-15 17:16:06 +0000

    seeded better and fixed some css

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,19 +35,69 @@ pub = Place.create(name: "The Pub")
 pub.placetype = Placetype.first
 pub.save
 
-50.times do |i|
-  if rand(10) >= 5 
-    Person.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, dob: Faker::Date.between(200.years.ago, Date.today))
-  else
-    born = Faker::Date.between(200.years.ago, Date.today)
-    Person.create(firstname: Faker::Name.first_name, middlenames: "The #{Faker::Name.title}", lastname: Faker::Name.last_name, dob: born, died: Faker::Date.between(born, Date.today))
-  end
-
-  Memory.create(title: Faker::App.name, story: Faker::Lorem.paragraph(2, false, 4), ranking: 1, user_id: admin.id)
+20.times do 
+	Charity.create(name: Faker::Company.name)
 end
 
+100.times do 
+	mem = Memory.create(title: Faker::App.name, story: Faker::Lorem.paragraph(2, false, 4), ranking: 1, user_id: admin.id)
+end
 
 spec = ['dog', 'cat', 'bird', 'rodent','fish', 'reptile', 'other']
 spec.each do |species|
   Species.create(name: species)
+end
+
+50.times do |i|
+  if rand(10) >= 5 
+    p = Person.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, dob: Faker::Date.between(200.years.ago, Date.today))
+  else
+    born = Faker::Date.between(200.years.ago, Date.today)
+    charity = Charity.limit(1).order("RANDOM()")[0]
+    p = Person.create(firstname: Faker::Name.first_name, middlenames: "The #{Faker::Name.title}", lastname: Faker::Name.last_name, dob: born, died: Faker::Date.between(born, Date.today))
+  	p.charity = charity
+  end
+  mem = Memory.limit(rand(3)).order("RANDOM()")
+  p.memories << mem
+end
+
+50.times do
+	born = Faker::Date.between(200.years.ago, Date.today)
+	spec = Species.limit(1).order("RANDOM()")[0]
+	pet = Pet.create(name: Faker::Name.first_name, dob: born, died: Faker::Date.between(born, Date.today))
+	pet.species = spec
+	pet.save
+end
+
+50.times do
+	ptid = Placetype.limit(1).order("RANDOM()")[0].id
+	lat = Faker::Address.latitude
+	lng = Faker::Address.longitude
+	Place.create(name: Faker::Company.name, placetype_id: ptid, address: Faker::Address.street_address, lat: "#{lat}", lng: "#{lng}")
+end
+
+20.times do
+	if rand(10) >= 5
+		Occasion.create(date: Faker::Date.between(100.years.ago, Date.today), name: Faker::Lorem.word)
+	else
+		date = Faker::Time.between(100.years.ago, Date.today)
+		Occasion.create(date: date, time: date, name: Faker::Lorem.word)
+	end
+end
+
+200.times do
+	g = Group.new
+	g.name = Faker::Lorem.word
+	g.about = Faker::Lorem.sentences
+
+	ppl = Person.limit(rand(3)).order("RANDOM()")
+	plc = Place.limit(rand(3)).order("RANDOM()")
+	occ = Occasion.limit(rand(3)).order("RANDOM()")
+	pet = Pet.limit(rand(3)).order("RANDOM()")
+
+	g.people << ppl unless ppl==nil
+	g.places << plc unless plc==nil
+	g.occasions << occ unless occ==nil
+	g.pets << pet unless pet ==nil
+	g.save
 end
