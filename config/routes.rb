7ff21371494bd90776d011af64f18494dfebb073
biglam <craig@techiteasy.org.uk>
2016-01-15 13:11:17 +0000

    userpage added to menu

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'memories#index'
+  root 'people#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
 resources :users, :only => [:show]
   # Example of regular route:
