97364d2f0d1b8a5c6fdf0a7ec7483f89979c0aca
biglam <craig@techiteasy.org.uk>
2016-01-23 14:40:41 +0000

    tweaked fornt page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,9 @@ class PagesController < ApplicationController
 	def welcome
     @lastfive = Memory.all.order('updated_at DESC').first(5)
     @yourmems = []
+    @topranked = Memory.all.order('ranking DESC').first(5)
+    # @mosttalked = People.all.order('')
+    @justadded = Person.all.order('created_at DESC').first(5)
     if user_signed_in?
       @yourmems = current_user.memories.order('updated_at DESC').first(5)
     end
