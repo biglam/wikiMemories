df38a968d3f160d3ddb742f393c3b1a6942cd10c
biglam <craig@techiteasy.org.uk>
2016-01-25 14:55:00 +0000

    places model working

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,7 @@ class Place < ActiveRecord::Base
 	has_and_belongs_to_many :groups
 	has_many :place_administrations
 	has_many :administrators, through: :place_administrations
+	has_many :images, :as => :image_item
 	validates :name, presence: true
 
 	def get_lat_lng_from_address
@@ -14,6 +15,23 @@ class Place < ActiveRecord::Base
 			self.lng = latlng['lng']
 			self.save
 		end
+	end
+
+	def get_top_image
+		self.images.order('ranking DESC')[0].image
+	end
+
+	def can_admin(user)
+		if user
+			list = self.administrators.map {|admin| admin.id}
+			list.include?(user.id)
+		end
+	end
+
+	def remove_item(item)
+		# binding.pry;''
+		self.administrators.delete(item) if item.class == User
+		self.save
 	end
 
 end
