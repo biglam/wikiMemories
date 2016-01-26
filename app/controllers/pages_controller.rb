282396aa73e7dd0160da1afa2c765c49716ca3cf
biglam <craig@techiteasy.org.uk>
2016-01-26 21:10:33 +0000

    commenting out anniversatry stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,9 +10,10 @@ class PagesController < ApplicationController
     @topranked = Memory.all.order('ranking DESC').first(5)
     # @mosttalked = People.all.order('')
     @justadded = Person.all.order('created_at DESC').first(5)
-    @anniversaries = Person.where('dob BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
-    @anniversaries +=  Person.where('dob BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
-    @anniversaries +=  Occasion.where('date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    # @anniversaries = Person.where(:dob.strftime("%d%m") == DateTime.new.strftime("%d%m")).all
+    # @anniversaries +=  Person.where('dob BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    # @anniversaries +=  Occasion.where('date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    @anniversaries = 
     if user_signed_in?
       @yourmems = current_user.memories.order('updated_at DESC').first(5)
     end
@@ -26,3 +27,5 @@ class PagesController < ApplicationController
 
   end
 end
+
+Person.last.dob.strftime("%d%m") == DateTime.new.strftime("%d%m")
