1a0c41a9b38542ec9ed900c4e68df5ad52af89f8
biglam <craig@techiteasy.org.uk>
2016-01-26 21:10:00 +0000

    started bdays but will come back later

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,9 @@ class PagesController < ApplicationController
     @topranked = Memory.all.order('ranking DESC').first(5)
     # @mosttalked = People.all.order('')
     @justadded = Person.all.order('created_at DESC').first(5)
+    @anniversaries = Person.where('dob BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    @anniversaries +=  Person.where('dob BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
+    @anniversaries +=  Occasion.where('date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
     if user_signed_in?
       @yourmems = current_user.memories.order('updated_at DESC').first(5)
     end
