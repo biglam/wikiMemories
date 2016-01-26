ec8137deea5cc0e5b61a30b6708874c065de4cbe
biglam <craig@techiteasy.org.uk>
2016-01-26 22:30:32 +0000

    people table sortable

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,12 +2,13 @@ class PeopleController < ApplicationController
 
   before_action :set_person, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
+  helper_method :sort_column, :sort_direction
 
   def index
     if params[:search]
       @people_search_result = Person.where("lastname like ?", "%#{params[:search]}%").limit(5) if params[:search] > ""
     else
-      @people = Person.all.paginate(:page => params[:page], :per_page => 10)
+      @people = Person.all.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 10)
     end
     render @people_search_result, layout: false if request.xhr?
   end
@@ -113,4 +114,13 @@ class PeopleController < ApplicationController
   def person_params
     params.require(:person).permit(:firstname, :middlenames, :lastname, :dob, :died, :charity_id, :place_of_birth, :brief_description)
   end
+
+  def sort_column
+    Person.column_names.include?(params[:sort]) ? params[:sort] : "lastname"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
